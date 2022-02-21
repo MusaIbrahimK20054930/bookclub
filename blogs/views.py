648f9571550fb78d6b39e44a50549c66647b53ac
Feisal <feisal.elmi@kcl.ac.uk>
2022-02-21 17:08:33 +0000

    add create club

--- blogs/views.py
+++ blogs/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from .forms import SignUpForm
-from .forms import SignInForm
+from .forms import SignUpForm, SignInForm, ClubCreationForm
 from django.contrib.auth import authenticate, login, logout
+from .models import Club
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
-def info(request):
-    return render(request, 'info.html')
+def group(request):
+    return render(request, 'group.html')
 
 
 def sign_up(request):
@@ -21,7 +22,7 @@ def sign_up(request):
             user.set_password(form.cleaned_data['password_confirmation'])
             user.save()
             login(request, user)
-            return redirect('info')
+            return redirect('group')
     else:
         form = SignUpForm()
     return render(request, 'sign_up.html', {'form': form})
@@ -36,7 +37,22 @@ def sign_in(request):
             user = authenticate(username=username, password=passwordEnter)
             if user is not None:
                 login(request, user)
-                return redirect('info')
+                return redirect('group')
 
     form = SignInForm()
     return render(request, 'sign_in.html', {'form': form})
+
+def club_creation(request):
+    """Allow user to create club."""
+    if request.method == 'POST':
+        form = ClubCreationForm(request.POST)
+        if form.is_valid():
+            # The club-creation form is valid, we save the club to the database
+            form.save()
+            messages.add_message(request, messages.SUCCESS, "Club created successfully.")
+            return redirect('group')
+        else:
+            messages.add_message(request, messages.ERROR, "This club name is already taken, please choose another one.")
+    else:
+        form = ClubCreationForm(initial = {'owner': request.user})
+    return render(request, 'new_club.html', {'form': form})
--- bookclub/urls.py
+++ bookclub/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('sign_up/', views.sign_up, name = 'sign_up'),
     path('sign_in', views.sign_in, name = 'sign_in'),
-    path('info', views.info, name = 'info'),
+    path('group', views.group, name = 'group'),
+    path('new_club/', views.club_creation, name='new_club'),
+
 ]
