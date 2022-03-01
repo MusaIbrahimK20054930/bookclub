b715cd7e945cd48178ed14d082cd5dd5d013eb0e
MusaIbrahimK20054930 <81184552+MusaIbrahimK20054930@users.noreply.github.com>
2022-03-01 12:54:35 +0000

    Fix setting.py

--- bookclub/settings.py
+++ bookclub/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs',
-    'widget_tweaks',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
