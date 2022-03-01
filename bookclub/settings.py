a1550b4bc69a6cd3bbede62e10f268756b191a22
MusaIbrahimK20054930 <81184552+MusaIbrahimK20054930@users.noreply.github.com>
2022-03-01 13:08:47 +0000

    Fix setting.py
    fix setting.py to able server to run

--- bookclub/settings.py
+++ bookclub/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs',
-    ]
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
